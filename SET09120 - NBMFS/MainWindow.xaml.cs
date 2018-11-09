78542eb333315b23cbf7ff302b3c63cfe7cc0388
Aidan McKenzie <40203712@live.napier.ac.uk>
2018-11-09 20:04:49 +0000

    Added case statement dependent on message type, as well as some functionality.

--- SET09120 - NBMFS/MainWindow.xaml.cs
+++ SET09120 - NBMFS/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using System.Windows.Forms;
 
 namespace SET09120___NBMFS
 {
@@ -20,9 +21,64 @@ namespace SET09120___NBMFS
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string header;
+        public string messageType;
+        public string sender;
+        public string subject;
+        public string body;
+        
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /*private void txtHeader_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+
+        }*/
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            header = txtHeader.Text;
+            messageType = header.Substring(0, 1).ToUpper();
+            sender = txtSender.Text;
+            //subject = txtSubject.Text
+            body = txtBody.Text;
+
+
+            if (header.Length == 9)
+            {
+                switch (messageType)
+                {
+                    case "S":
+                        MessageBox.Show("SMS!");
+
+                        if (body.Length > 0 && body.Length <= 140)
+                        {
+                            MessageBox.Show(body);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please ensure the SMS body is between 0 and 140 characters.");
+                        }
+                        break;
+                    case "E":
+                        MessageBox.Show("Email!");
+                        break;
+                    case "T":
+                        MessageBox.Show("Tweet!");
+                        break;
+                    default:
+                        MessageBox.Show("Please enter a valid message header.");
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a message header that is a single character followed by nine numbers. \n e.g.: 'E123456789'");
+            }
+
         }
     }
 }
